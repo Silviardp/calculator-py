17fc19828ab56234c02981253890243ab3c2ff84
Silvia Rocha <silviadprocha@gmail.com>
2020-09-01 22:14:09 +0200

    improve code

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
+def welcome():
+    print('''
+Welcome to Calculator
+''')
+
 
 def calculate():
     operation = input('''
@@ -6,6 +11,8 @@ Please type in the math operation you would like to complete:
 - for subtraction
 * for multiplication
 / for division
+** for power
+% for modulo
 ''')
 
     number_1 = int(input('Enter your first number: '))
@@ -31,6 +38,16 @@ Please type in the math operation you would like to complete:
         print('{} / {} = '.format(number_1, number_2))
         print(number_1 / number_2)
 
+    # Power
+    elif operation == '**':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 ** number_2)
+
+    # Modulo
+    elif operation == '%':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 % number_2)
+
     else:
         print('You have not typed a valid operator, please run the program again.')
 
@@ -51,4 +68,5 @@ def again():
         again()
 
 
+welcome()
 calculate()
